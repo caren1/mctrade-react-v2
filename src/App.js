cc8990b796f5c1c96a80e199723827e541c585be
caren1 <czarnocki.w@wp.pl>
2020-10-27 22:09:24 +0100

    initialized styled components and Navbar component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import './App.css';
 
+// Components :
+// - Navbar
+// - Button
+// - Info (o firmie i szukamy współpracy)
+// - Offer -> SingleOffer -> SingleOfferDetails
+// - Projects -> SingleProject -> SingleProjectDetails
+// - Footer -> Contact Form
+
 function App() {
   return (
     <div className="app">
